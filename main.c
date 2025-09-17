2e3700b7b6922f7e2a0cf9213f8965b38fc54338
Zeev Rotshtein <zeevro@gmail.com>
2025-09-17 04:54:48 +0300

    Improve basic flow (tested with wayland-py/examples/basic.py)

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ static void wl_compositor_create_surface_handler(struct wl_client *client, struc
         return;
     }
     struct wl_surface_data *data = calloc(1, sizeof(*data));
-    wl_resource_set_implementation(surface_resource, &wl_surface_interface_impl, data, (wl_resource_destroy_func_t)free);
+    wl_resource_set_implementation(surface_resource, &wl_surface_interface_impl, data, NULL);
 }
 
 static const struct wl_compositor_interface wl_compositor_interface_impl = {
@@ -60,6 +60,8 @@ static void wl_compositor_bind_handler(struct wl_client *client, void *data, uin
 static void wl_surface_attach_handler(struct wl_client *client, struct wl_resource *resource, struct wl_resource *buffer, int32_t x, int32_t y) {
     struct wl_surface_data *data = wl_resource_get_user_data(resource);
     data->buffer = buffer;
+
+    if (data->xdg_surface) xdg_surface_send_configure(data->xdg_surface, wl_display_next_serial(wl_client_get_display(client)));
 }
 
 static void wl_surface_commit_handler(struct wl_client *client, struct wl_resource *resource) {
@@ -125,7 +127,7 @@ static void xdg_wm_base_get_xdg_surface_handler(struct wl_client *client, struct
     wl_resource_set_implementation(xdg_surface, &xdg_surface_interface_impl, data, NULL);
     wl_resource_set_user_data(surface, data);
 
-    xdg_surface_send_configure(xdg_surface, 5678);
+    xdg_surface_send_configure(xdg_surface, wl_display_next_serial(wl_client_get_display(client)));
 }
 
 static const struct xdg_wm_base_interface xdg_wm_base_interface_impl = {
@@ -157,6 +159,11 @@ static void xdg_surface_get_toplevel_handler(struct wl_client *client, struct wl
         return;
     }
     wl_resource_set_implementation(data->xdg_toplevel, &xdg_toplevel_interface_impl, data, NULL);
+
+    int mystate = 0;
+    struct wl_array states = {sizeof(mystate), 0, &mystate};
+    xdg_toplevel_send_configure(data->xdg_toplevel, 0, 0, &states);
+    xdg_surface_send_configure(resource, wl_display_next_serial(wl_client_get_display(client)));
 }
 
 static void xdg_surface_ack_configure_handler(struct wl_client *client, struct wl_resource *resource, uint32_t serial) {
@@ -177,19 +184,36 @@ static const struct xdg_surface_interface xdg_surface_interface_impl = {
 // xdg_toplevel interface implementation
 // =========================================================================
 
+static void xdg_toplevel_destroy_handler(struct wl_client *client, struct wl_resource *resource) {}
+static void xdg_toplevel_set_parent_handler(struct wl_client *client, struct wl_resource *resource, struct wl_resource *parent) {}
+static void xdg_toplevel_set_title_handler(struct wl_client *client, struct wl_resource *resource, const char *title) {}
+static void xdg_toplevel_set_app_id_handler(struct wl_client *client, struct wl_resource *resource, const char *app_id) {}
+static void xdg_toplevel_show_window_menu_handler(struct wl_client *client, struct wl_resource *resource, struct wl_resource *seat, uint32_t serial, int32_t x, int32_t y) {}
+static void xdg_toplevel_move_handler(struct wl_client *client, struct wl_resource *resource, struct wl_resource *seat, uint32_t serial) {}
+static void xdg_toplevel_resize_handler(struct wl_client *client, struct wl_resource *resource, struct wl_resource *seat, uint32_t serial, uint32_t edges) {}
+static void xdg_toplevel_set_max_size_handler(struct wl_client *client, struct wl_resource *resource, int32_t width, int32_t height) {}
+static void xdg_toplevel_set_min_size_handler(struct wl_client *client, struct wl_resource *resource, int32_t width, int32_t height) {}
+static void xdg_toplevel_set_maximized_handler(struct wl_client *client, struct wl_resource *resource) {}
+static void xdg_toplevel_unset_maximized_handler(struct wl_client *client, struct wl_resource *resource) {}
+static void xdg_toplevel_set_fullscreen_handler(struct wl_client *client, struct wl_resource *resource, struct wl_resource *output) {}
+static void xdg_toplevel_unset_fullscreen_handler(struct wl_client *client, struct wl_resource *resource) {}
+static void xdg_toplevel_set_minimized_handler(struct wl_client *client, struct wl_resource *resource) {}
+
 static const struct xdg_toplevel_interface xdg_toplevel_interface_impl = {
-    .destroy = NULL,
-    .set_parent = NULL,
-    .set_title = NULL,
-    .set_app_id = NULL,
-    .show_window_menu = NULL,
-    .move = NULL,
-    .resize = NULL,
-    .set_maximized = NULL,
-    .unset_maximized = NULL,
-    .set_fullscreen = NULL,
-    .unset_fullscreen = NULL,
-    .set_minimized = NULL,
+    .destroy = xdg_toplevel_destroy_handler,
+    .set_parent = xdg_toplevel_set_parent_handler,
+    .set_title = xdg_toplevel_set_title_handler,
+    .set_app_id = xdg_toplevel_set_app_id_handler,
+    .show_window_menu = xdg_toplevel_show_window_menu_handler,
+    .move = xdg_toplevel_move_handler,
+    .resize = xdg_toplevel_resize_handler,
+    .set_max_size = xdg_toplevel_set_max_size_handler,
+    .set_min_size = xdg_toplevel_set_min_size_handler,
+    .set_maximized = xdg_toplevel_set_maximized_handler,
+    .unset_maximized = xdg_toplevel_unset_maximized_handler,
+    .set_fullscreen = xdg_toplevel_set_fullscreen_handler,
+    .unset_fullscreen = xdg_toplevel_unset_fullscreen_handler,
+    .set_minimized = xdg_toplevel_set_minimized_handler,
 };
 #pragma endregion
 
@@ -216,9 +240,8 @@ int main() {
 
     wl_display_add_protocol_logger(display, protocol_logger_func, NULL);
 
-    wl_display_init_shm(display);
-
     wl_global_create(display, &wl_compositor_interface, 4, NULL, wl_compositor_bind_handler);
+    wl_display_init_shm(display);
     wl_global_create(display, &xdg_wm_base_interface, 1, NULL, xdg_wm_base_bind_handler);
 
     fprintf(stderr, "Wayland display running on WAYLAND_DISPLAY=%s\n", socket_name);
